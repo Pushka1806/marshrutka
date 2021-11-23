1dd9de3a88bc555d2c518405e3c464f04b929275
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2021-11-23 10:36:29 +0300

    Update User_driver.js

--- models/User_driver.js
+++ models/User_driver.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const {Schema} = require("mongoose");
 const userSchema = new Schema({
-    email:{
+    login:{
         type: String,
         required: true,
         unique: true
