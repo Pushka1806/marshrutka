0ded691b0520c6c0a17a1d9d224c0fa5ed720527
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2021-11-23 23:50:44 +0300

    Update gps.js

--- controllers/gps.js
+++ controllers/gps.js
@@ -1,7 +1,18 @@
-module.exports.getAll = function (req, res){
+const Passenger = require('../models/User_passenger');
 
+//просто получить ВСЕ остановки - правил
+module.exports.driverRouteID = async function (req, res){
+    const getZakaz = await Passenger.find();
+    let sortedRoute = new Array();
+    for(const zakaz of getZakaz){
+        for (let i = 0;;i++) {
+            if(zakaz.routeID.i === req.query.routeID) {
+               sortedRoute.push(zakaz._id);   
+            }
+        }
+    }
+    //console.log(sortedDrivers);
+    const sortedRoute_res = {names: sortedRoute}
+    res.status(200).json(sortedRoute_res);
 }
-
-module.exports.update = function (req, res){
-
-}
+   
