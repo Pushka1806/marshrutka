04442af3f76e518a8ec06d738033f0f0b0aabf74
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2021-11-23 13:18:49 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ module.exports.login = async function (req, res){
     if(candidate){
         //нашли - проверяем пароль
      
-        const passwordResult = bcrypt.compareSync(req.body.login, candidate._id.login);
+        const passwordResult = bcrypt.compareSync(req.body.login, candidate."_id.login");
         if(passwordResult){
             //гененируем токен, т.к. пароль правильный
             //const token = jsonwebtoken.sign({
