8372debd91d286e7b96fce55b9827a91a8a08763
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2021-11-25 03:12:11 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,8 @@ router.post('/login', controller.login);
 //localhost:5000/api/auth/register
 router.post('/register', controller.register);
 
-router.get('/getInfo', controller.getInfo);
+router.get('/driverGetInfo', controller.driverGetInfo);
+
+router.get('/driverGetRouteById', controller.driverGetRouteById);
 
 module.exports = router;
