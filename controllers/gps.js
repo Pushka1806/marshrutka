45f690ad5d00c33c47a449cca662fa57705c6629
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2021-11-26 13:26:29 +0300

    Update gps.js

--- controllers/gps.js
+++ controllers/gps.js
@@ -7,17 +7,16 @@ module.exports.AvailableZakaz = async function (req, res){
     let sortedRoute = new Array();
     for(const zakaz of getZakaz){
         for (const route  of zakaz.routeID) {
-            let startstop = new Array();
             if(route === req.query.routeID) {
-               startstop.push({start:zakaz.start, stop:zakaz.stop});
-               sortedRoute.push(startstop);
+               sortedRoute.push({start:zakaz.start, stop:zakaz.stop});
+               
       
             }
            
         }
     }
     //console.log(sortedDrivers);
-    const sortedRoute_res = {stops: sortedRoute}
-    res.status(200).json(sortedRoute_res);
+   // const sortedRouteRoute = {stops: sortedRoute}
+    res.status(200).json(sortedRoute);
 }
    
