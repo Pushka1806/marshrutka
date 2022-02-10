8bbbbb4e4e22ac000b2303c58e874c40236ac30c
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2022-02-10 17:26:58 +0300

    Update gps.js

--- routes/gps.js
+++ routes/gps.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/gps');
 const router = express.Router();
 
 router.get('/AvailableZakaz', controller.AvailableZakaz);
+router.get('/getJpsByStops', controller.getJpsByStops);
 
 module.exports = router;
