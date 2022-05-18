65bdc345d2a27db1232ea42e299f4bd88ab7a61f
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2022-05-18 14:11:03 +0300

    Update gps.js

--- controllers/gps.js
+++ controllers/gps.js
@@ -52,15 +52,13 @@ module.exports.getJpsByStops = async function(req, res) {
 
 module.exports.getGpsDriver = async function(req, res) {
     let ok_cars = new Array();
-    for( let route of  req.query.routes){
-       
+    for( let route of  req.body.routeID){
         let cars_route = await Driver.find({route_work:route}); // получили водителей работающих на этом маршруте
         const driver_route = await Driver_route.findOne({_id:route}); // получили остановки маршрута
         let okDriverByRoute = new Array();
-        
         for(let car of cars_route){
             //res.status(200).json(check_gps(car,driver_route,req.query.start));
-             if(check_gps(car,driver_route,req.query.start)){ // проверяем, приедет ли водитель на нашу остановку или уже проехал
+             if(check_gps(car,driver_route,req.body.start)){ // проверяем, приедет ли водитель на нашу остановку или уже проехал
                  let lat = car.gps.latitude;
                  let lon = car.gps.longitude;
                  let car_result = { id: car._id,
